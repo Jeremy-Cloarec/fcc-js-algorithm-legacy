0a3c19a3219b8415563a48845946276d7be97c27
Jeremy Cloarec <jeremycloarec@msn.com>
2024-10-04 12:29:19 +0200

    cash register

--- cash_register/index.js
+++ cash_register/index.js
@@ -1,98 +1,115 @@
 function checkCashRegister(price, cash, cid) {
     //calculate rest
     let rest = cash - price
-    //object with conversion
-    const conversion = {
-        "HUNDRED": 100,
-        "TWENTY": 20,
-        "TEN": 10,
-        "FIVE": 5,
-        "ONE": 1,
-        "QUARTER": 0.25,
-        "DIME": 0.1,
-        "NICKEL": 0.05,
-        "PENNY": 0.01,
+    //array with conversion
+    const conversion = [
+        ["PENNY", 0.01],
+        ["NICKEL", 0.05],
+        ["DIME", 0.1],
+        ["QUARTER", 0.25],
+        ["ONE", 1],
+        ["FIVE", 5],
+        ["TEN", 10],
+        ["TWENTY", 20],
+        ["ONE HUNDRED", 100],
+    ]
+
+    const response = {}
+    response.status = "OPEN"
+    response.change = []
+
+
+    let usingDevises = []
+    let conversionToUse = conversion.reverse().find((ele => ele[1] <= rest))
+
+    function recursiveCid() {
+        //out of the recursive functio
+        if (rest <= 0) {
+            console.log("go out of the function");
+            return response
+        }
+
+        //substract the devise in cid with the devise
+        let checkInCid = cid.find(ele => ele[0] === conversionToUse[0])
+
+        if (checkInCid[1] - conversionToUse[1] < 0 || rest - conversionToUse[1] < 0) {
+            for (let i = 0; i < conversion.length; i++) {
+
+                let cidReverse = [...cid].reverse()
+                if (conversion[i][1] <= rest && cidReverse[i][1] != 0) {
+                    conversionToUse = conversion[i]
+                    break
+                }
+
+                if(conversionToUse[0] === "PENNY" && checkInCid[1] === 0 && rest >= 0 ) {
+                    console.log("last devise");
+                    response.status = "INSUFFICIENT_FUNDS"
+                    return response
+                }
+            }
+        }
+
+        if (checkInCid[1] - conversionToUse[1] >= 0) {
+
+            checkInCid[1] -= conversionToUse[1]
+            checkInCid[1] = Math.round(checkInCid[1] * 100) / 100
+            usingDevises.push([...conversionToUse])
+            rest -= conversionToUse[1]
+            rest = Math.round(rest * 100) / 100
+        }
+
+        recursiveCid()
     }
 
-    //recursive function to gave cash
-    function recursiveCash(restParam) {
-        if (restParam <= 0) {
-            return
-        } else {
+    recursiveCid()
 
-            chooseMoney(restParam)
-
-            // check minimum device to give
-            function chooseMoney(restParam) {
-                for (const conv in conversion) {
-                    if (restParam >= conversion[conv]) {
-                        console.log(conv, conversion[conv])
-                        
-                        const amountDevise = checkInCid(conv)
-                        console.log("amountdevise: " + amountDevise)
-                        console.log("restParam: " + restParam);
-
-                        if (amountDevise > restParam) {
-                            const restAmountDevise = substractCID(amountDevise, restParam)
-                            console.log(amountDevise + "-" + restParam);
-                            console.log("restAmountDevise: " + restAmountDevise)
-
-                            //here push the the new value in the array after substract
-                            updatedCid(cid, conv, restAmountDevise)
-                            console.log("newArray: " + cid)
-
-                            rest -= conversion[conv]
-                            break;
-
-                        } else {
-                            console.log("oups");
-                            return
-                        }
-                    }
-                }
-            }
-
-            //check the amount of these devise in the cid
-            function checkInCid(devise) {
-                for (const c of cid) {
-                    if (c[0] === devise) {
-                        return c[1]
-                    }
-                }
-            }
-            function checkInCidAfter(arrayCid, devise, index) {
-                for(let i = 0; i < arrayCid.length; i++ ) {
-                    if (arrayCid[i][0] === devise) {
-                        return
-                    }
-                }
-            }
-
-            //substract amount for cash
-            function substractCID(cash, amount) {
-                if (cash > amount) {
-                    return cash - amount
-                } else {
-                    console.log("not enought");
-                }
-            }
-
-            //update cid with the new value
-            function updatedCid(arrayCID, devise, newValue) {
-                for (const c of arrayCID) {
-                    if (c[0] === devise) {
-                        console.log("devise to change: " + c[1]);
-                        c[1] = newValue
-                    }
-                }
-            }
-
-            console.log(rest)
-            recursiveCash(rest)
+    for (let i = 0; i < usingDevises.length; i++) {
+        if (usingDevises[i - 1] && usingDevises[i - 1][0] === usingDevises[i][0]) {
+            usingDevises[i][1] += usingDevises[i - 1][1]
+            usingDevises[i - 1] = undefined
         }
     }
-    recursiveCash(rest)
-    return;
+
+    let usingDevisesReduced = usingDevises.filter(ele => ele !== undefined)
+    usingDevisesReduced = usingDevisesReduced.map(ele => {
+        Math.round(ele[1] * 100) / 100
+        return ele
+    })
+
+    response.change = [...usingDevisesReduced]
+
+    if (response.status === "INSUFFICIENT_FUNDS") {
+        response.change = []
+    } 
+    
+    if (cid.every(ele => ele[1] === 0) && rest === 0) {        
+        cid.map(ele => {
+            if(ele[0] === response.change[0][0] ){
+                ele[1] = Math.round(response.change[0][1] * 100) / 100  
+            }
+        })
+
+        response.status = "CLOSED"
+        response.change = [...cid]
+        return response
+    } 
+
+    return response
 }
 
-checkCashRegister(19.5, 60, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
+console.log(checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]))
+
+//{status: "OPEN", change: [["QUARTER", 0.5]]}.
+
+//console.log(checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]))
+
+//{status: "OPEN", change: [["TWENTY", 60], ["TEN", 20], ["FIVE", 15], ["ONE", 1], ["QUARTER", 0.5], ["DIME", 0.2], ["PENNY", 0.04]]}.
+
+//console.log(checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]))
+//{status: "INSUFFICIENT_FUNDS", change: []}.
+
+//console.log(checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]))
+//{status: "INSUFFICIENT_FUNDS", change: []}.
+
+//console.log(checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]))
+//{status: "CLOSED", change: [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]}
